905a334b50d3f2a493c9031ece88ce0910032ee6
Linus Liang <LinusLiang@protonmail.com>
2019-11-15 13:05:17 -0500

    1

--- grader.js
+++ grader.js
@@ -1,21 +1,31 @@
 class GradingService {
+  constructor() {}
 
-  constructor(){
-
+  normalizeGradeValue(gradeValue, scoreCodes) {
+    if(isNaN(parseInt(gradeValue))) {
+      const scoreCode = scoreCodes.filter(item => item.code === gradeValue);
+      return scoreCode.percent;
+    } else {
+      return parseInt(gradeValue);
+    }
   }
 
+  gradeAssignments(assignments, grades, scoreCodes) {
+    return grades.map(grade => {
+      const assignment = assignments.filter(item => item.id === grade.assignmentId);
+      
+      const percent = this.normalizeGradeValue(grade.grade, scoreCodes) / assignment.maxScore;
 
-  calculate(assignments, grades){
-      /*
-        1. loop over all the graders
-        2. find the assignment this grade belongs to
-        3. compute score
-      */
+      return {
+        maxPoints: assignment.points,
+        earnedPoints: percent * assignments.points,
+        grade: percent,
+        categoryId: assignment.categoryId,
+        maxScore: assignment.maxScore,
+        studentId: grade.studentId
+      }
+    });
   }
 
+  
 }
-
-
-const grader = new GradingService();
-
-const gradedAssignments = grader.calculate(assignments, grades);
