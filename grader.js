4233cf9b01d2710817f33b62a30b67f516939c8d
Linus Liang <LinusLiang@protonmail.com>
2019-11-18 06:15:41 -0500

    2

--- grader.js
+++ grader.js
@@ -1,9 +1,19 @@
+Array.prototype.isEmpty = function () {
+  return this.length === 0;
+};
+
+Array.prototype.add = function () {
+  return this.reduce((left, right) => left + right);
+}
+
 class GradingService {
-  constructor() {}
+  constructor() {
+    this.worthlessGrade = [0, 0];
+  }
 
   normalizeGradeValue(gradeValue, scoreCodes) {
     if(isNaN(parseInt(gradeValue))) {
-      const scoreCode = scoreCodes.filter(item => item.code === gradeValue);
+      const [scoreCode] = scoreCodes.filter(scoreCode => scoreCode.code === gradeValue);
       return scoreCode.percent;
     } else {
       return parseInt(gradeValue);
@@ -12,20 +22,50 @@ class GradingService {
 
   gradeAssignments(assignments, grades, scoreCodes) {
     return grades.map(grade => {
-      const assignment = assignments.filter(item => item.id === grade.assignmentId);
+      const [assignment] = assignments.filter(item => item.id === grade.assignmentId);
       
-      const percent = this.normalizeGradeValue(grade.grade, scoreCodes) / assignment.maxScore;
+      const score = this.normalizeGradeValue(grade.grade, scoreCodes) / assignment.maxScore;
 
       return {
         maxPoints: assignment.points,
-        earnedPoints: percent * assignments.points,
-        grade: percent,
+        earnedPoints: score * assignment.points,
+        grade: score,
         categoryId: assignment.categoryId,
         maxScore: assignment.maxScore,
-        studentId: grade.studentId
+        userId: grade.userId
       }
     });
   }
 
-  
+  calculateFinalGradeValues(grade) {
+    if (grade.maxScore === 0) {
+      return this.worthlessGrade;
+    }
+    return [grade.earnedPoints, grade.maxScore];
+  }
+
+  gradeStudentsCategories(students, categories, gradedAssignments) {
+    return students.map(student => {
+      return categories.map(category => {
+        const grades = gradedAssignments.filter(grade => grade.categoryId === category.id && grade.userId === student.userId);
+        
+        if (grades.isEmpty()) {
+          return 0;
+        }
+
+        grades[0] = this.calculateFinalGradeValues(grades[0]);
+
+        const accGradeValues = grades.reduce((previousGradeValues, next) => {
+          const nextGradeValues = this.calculateFinalGradeValues(next);
+
+          previousGradeValues[0] += nextGradeValues[0];
+          previousGradeValues[1] += nextGradeValues[1];
+          return nextGradeValues;
+        });
+
+        const categoryAverage = accGradeValues[0] / accGradeValues[1];
+        return categoryAverage;
+      });
+    });
+  }
 }
