50c5f122123f6d3b62dbe81068cd78f1609819c3
Linus Liang <LinusLiang@protonmail.com>
2019-12-17 12:56:29 -0500

    31

--- ViewModel.js
+++ ViewModel.js
@@ -8,7 +8,7 @@ function Grade(grade, score) {
 function ViewModel() {
     const self = this;
 
-    const _gradingService = new GradingService();
+    const _gradingService = new GradingService(GradingSchemas.letter);
 
     self.student = ko.observable();
 
@@ -25,26 +25,27 @@ function ViewModel() {
 
     self.overallGrade = ko.observable();
 
-    self.updateAssignmentEntry = function(assignmentEntry) {
-        const grade = self.grades().find(g => g.assignmentId === assignmentEntry.assignmentId);
+    self.updateAssignmentEntry = function(newGrade) {
+        // this = an AssignmentEntry form self.assignmentView
+        const grade = self.grades().find(g => g.assignmentId === this.assignmentId);
         const assignment = self.assignments().find(a => a.id === grade.assignmentId);
         
-        grade.grade = assignmentEntry.earnedScore(); // Update the score of the grade so it can be graded properly by the grader service
+        grade.grade = newGrade; // Update the score of the grade so it can be graded properly by the grader service
 
-
-        let currentGradedAssignment = self.gradedAssignments().find(ga => ga.assignmentId === assignmentEntry.assignmentId); 
+        let currentGradedAssignment = self.gradedAssignments().find(ga => ga.assignmentId === this.assignmentId); 
         const newGradedAssignment = _gradingService.gradeAssignment(assignment, grade, self.scoreCodes());
-        // if (newGradedAssignment) {
-            // currentGradedAssignment = {...newGradedAssignment}
-        Object.assign(currentGradedAssignment, newGradedAssignment); // Update the properties of the old assignment with the new recalculated assignment
-        // } else {
-        //     for (const key of Object.keys(currentGradedAssignment)) {
-        //         if (key == )
-        //         currentGradedAssignment[key] = undefined;
-        //     } // When 
-        // }
+        if (newGradedAssignment) {
+            Object.assign(currentGradedAssignment, newGradedAssignment); // Update the properties of the old assignment with the new recalculated assignment
+        } else {
+            for (const key of Object.keys(currentGradedAssignment)) {
+                if (key == 'assignmentId') {
+                    continue;
+                }
+                currentGradedAssignment[key] = '';
+            } // When 
+        }
 
-        assignmentEntry.update(currentGradedAssignment);
+        this.update(currentGradedAssignment);
         self.assignmentView.notifySubscribers(); // Update the UI
         
         self.updateSummery();
@@ -85,19 +86,25 @@ function ViewModel() {
             /* This is done so assignments without a grade, can be edited */
             const grade = grades.find(g => g.assignmentId == a.id);
             if(!grade) {
-                return new Grade({ assignmentId: a.id, userId: student.userId }, 0);
+                return new Grade({ assignmentId: a.id, userId: student.userId }, '');
             }
+            // Regular: assignments with a grade
             return new Grade(grade, grade.grade);
         });
-        
-        
         self.grades(newGrades);
 
         self.gradedAssignments(_gradingService.gradeAssignments(
             self.assignments(),
-            self.grades(), 
+            self.grades(),
             self.scoreCodes()
         ));
+        for(const grade of self.grades()) {
+            /* When their is no grade for an assignment, the grader refuses to grade is, */
+            const possibleGradedAssignment = self.gradedAssignments().find(ga => ga.assignmentId === grade.assignmentId);
+            if(!possibleGradedAssignment) {
+                self.gradedAssignments.push({});
+            }
+        }
 
         self.assignmentView(self.assignments().map(a => {
             /* Creating the AssignmentView */
@@ -108,7 +115,7 @@ function ViewModel() {
         }));
 
         for (const assignmentEntry of self.assignmentView()) {            
-            assignmentEntry.earnedScore.subscribe(() => self.updateAssignmentEntry(assignmentEntry));
+            assignmentEntry.earnedScore.subscribe(self.updateAssignmentEntry, assignmentEntry);
             assignmentEntry.earnedScore.extend({ rateLimit: { timeout: 300, method: 'notifyWhenChangesStop' } });
         }
 
--- grader.js
+++ grader.js
@@ -1,19 +1,18 @@
+const GradingSchemas = {
+    letter: {
+        name: '',
+        ranges: [
+            { letterGrade: 'A', minGrade: 90, maxGrade: 100, color: 'green'},
+            { letterGrade: 'B', minGrade: 80, maxGrade: 89, color: 'blue'},
+            { letterGrade: 'C', minGrade: 70, maxGrade: 79, color: 'yellow'},
+            { letterGrade: 'F', minGrade:  0, maxGrade: 69, color: 'red'}
+        ]
+    }
+};
+
 class GradingService {
-    constructor() {
-        // grades that cannot be calculated for: having an invaild grade, or having a max score of 0
-        this.unGraded = [];
-        this.schemas = {
-            default: { letterGrade: '' },
-            letter: {
-                name: '',
-                ranges: [
-                    { letterGrade: 'A', minGrade: 90, maxGrade: 100, color: 'green'},
-                    { letterGrade: 'B', minGrade: 80, maxGrade: 89, color: 'blue'},
-                    { letterGrade: 'C', minGrade: 70, maxGrade: 79, color: 'yellow'},
-                    { letterGrade: 'F', minGrade:  0, maxGrade: 69, color: 'red'}
-                ]
-            }
-        };
+    constructor(schema) {
+        this.schema = schema; // used when calculating the letter grade
     }
 
     getLetterGrade(schema, number) {
@@ -25,62 +24,61 @@ class GradingService {
         }
     }
 
-    gradeAssignment(assignment, grade, scoreCodes) {
-        const gradedAssignment = {
-            id: grade.id,
-            categoryId: assignment.categoryId,
-            assignmentId: assignment.id,
-            userId: grade.userId,
-            extraCredit: assignment.extraCredit,
-            exempt: false,
-            maxScore: assignment.maxScore,
-            maxPoints: assignment.points,
-        };
-
-        let numericGrade = parseInt(grade.grade);
-        if (isNaN(numericGrade)) {
-            const scoreCode = scoreCodes.find(scoreCode => scoreCode.code === grade.grade);
-            if (scoreCode) {
-                numericGrade = scoreCode.percent / 100 * gradedAssignment.maxScore;
-                gradedAssignment.exempt = scoreCode.exempt;
-            } else {
-                this.unGraded.push(gradedAssignment);
-                console.warn('Ungraded assignment, cause: scoreCode does not exist or grade is not a number', gradedAssignment, assignment.name, 'grade:', numericGrade);
-                return;
-            }
-        }
-
-        gradedAssignment.earnedScore  = numericGrade;
-        gradedAssignment.grade        = gradedAssignment.earnedScore / gradedAssignment.maxScore;
-
-        gradedAssignment.earnedPoints = gradedAssignment.grade * gradedAssignment.maxPoints;
-        gradedAssignment.percentage = gradedAssignment.grade * 100;
-
-        if (assignment.maxScore === 0) {
-            this.unGraded.push(gradedAssignment);
-            console.warn('Ungraded assignment, cause: a maxScore of 0', gradedAssignment, assignment.name);
-            return;
-        }
-
-        if (gradedAssignment.maxPoints < 0 && gradedAssignment.maxScore     < 0 &&
-            gradedAssignment.grade     < 0 && gradedAssignment.earnedPoints < 0) {
-            this.unGraded.push(gradedAssignment);
-            console.warn('Ungraded assignment, cause: negative value(s)', gradedAssignment, assignment.name);
-            return;
-        }
-
-        return gradedAssignment;
+    gradeAssignment(assignment, grade, scoreCodes, ignoreWarnings) {
+        // gradeAssignments takes in 2 arrays and returns an array; structuring into array then destructuring
+        const [graded] = this.gradeAssignments([assignment], [grade], scoreCodes);
+        return graded;
     }
 
     //
-    gradeAssignments(assignments, grades, scoreCodes) {
+    gradeAssignments(assignments, grades, scoreCodes, ignoreWarnings) {
         const gradedAssignments = [];
         for (const grade of grades) {
             const assignment = assignments.find(item => item.id === grade.assignmentId);
-            const gradedAssignment = this.gradeAssignment(assignment, grade, scoreCodes);
-            if (gradedAssignment) {
-                gradedAssignments.push(gradedAssignment);
+
+            const gradedAssignment = {
+                id: grade.id,
+                categoryId: assignment.categoryId,
+                assignmentId: assignment.id,
+                userId: grade.userId,
+                extraCredit: assignment.extraCredit,
+                exempt: false,
+                maxScore: assignment.maxScore,
+                maxPoints: assignment.points,
+            };
+    
+            let numericGrade = parseInt(grade.grade);
+            if (isNaN(numericGrade)) {
+                const scoreCode = scoreCodes.find(scoreCode => scoreCode.code === grade.grade);
+                if (scoreCode) {
+                    numericGrade = scoreCode.percent / 100 * gradedAssignment.maxScore;
+                    gradedAssignment.exempt = scoreCode.exempt;
+                } else {
+                    console.warn('Ungraded assignment, cause: scoreCode does not exist or grade is not a number', gradedAssignment, assignment.name, 'grade:', numericGrade);
+                    continue;
+                }
             }
+    
+            gradedAssignment.earnedScore  = numericGrade;
+            gradedAssignment.grade        = gradedAssignment.earnedScore / gradedAssignment.maxScore;
+    
+            gradedAssignment.earnedPoints = gradedAssignment.grade * gradedAssignment.maxPoints;
+            gradedAssignment.percentage = gradedAssignment.grade * 100;
+
+            gradedAssignment.letterGrade = this.getLetterGrade(this.schema, gradedAssignment.percentage);
+    
+            if (assignment.maxScore === 0) {
+                console.warn('Ungraded assignment, cause: a maxScore of 0', gradedAssignment, assignment.name);
+                continue;
+            }
+    
+            if (gradedAssignment.maxPoints < 0 && gradedAssignment.maxScore     < 0 &&
+                gradedAssignment.grade     < 0 && gradedAssignment.earnedPoints < 0) {
+                console.warn('Ungraded assignment, cause: negative value(s)', gradedAssignment, assignment.name);
+                continue;
+            }
+
+            gradedAssignments.push(gradedAssignment);
         }
         return gradedAssignments;
     }
